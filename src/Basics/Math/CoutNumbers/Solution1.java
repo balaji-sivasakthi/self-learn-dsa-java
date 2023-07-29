37ce5235d06c8318d99b215f8a5ac0aa31facee4
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-07-29 19:06:59 +0530

    feat(count): added solution1 for count number

--- src/Basics/Math/CoutNumbers/Solution1.java
+++ src/Basics/Math/CoutNumbers/Solution1.java
@@ -1,7 +1,20 @@
 package Basics.Math.CoutNumbers;
 
+import java.util.Scanner;
 public class Solution1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int digits = 0;
+        while(number!=0){
+            ++digits;
+            number = number / 10;
+        }
+        System.out.println("Number of Digits "+ digits);
 
+        /*
+        *     Time Complexity =  o(n)
+        *     Space Complexity = O(1)
+        * */
     }
 }
