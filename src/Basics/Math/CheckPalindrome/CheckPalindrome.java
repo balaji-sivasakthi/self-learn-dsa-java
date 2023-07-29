a129619665e1c924adadbe18a16c95598cced52a
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-07-29 19:46:32 +0530

    feat(math): added check palindrome

---
+++ src/Basics/Math/CheckPalindrome/CheckPalindrome.java
@@ -0,0 +1,17 @@
+package Basics.Math.CheckPalindrome;
+
+import java.util.Scanner;
+
+public class CheckPalindrome {
+    public static void main(String[] args) {
+        Scanner scanner  = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int temp = number;
+        int reverse = 0;
+        while(number!=0){
+            reverse = reverse * 10 + number % 10;
+            number  = number / 10;
+        }
+        System.out.println("Is this Palindrome? "+((temp == reverse)?"Yes":"No"));
+    }
+}
