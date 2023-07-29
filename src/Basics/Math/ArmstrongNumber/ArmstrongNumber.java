5c94ed9cdacdcdae4bd6c074fc19a31d7faf3832
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-07-29 20:35:22 +0530

    feat(math): added armstrong number

---
+++ src/Basics/Math/ArmstrongNumber/ArmstrongNumber.java
@@ -0,0 +1,22 @@
+package Basics.Math.ArmstrongNumber;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int digits = (int) Math.floor(Math.log10(number)+1);
+        int sum = 0;
+        int temp = number;
+        while (number!=0){
+            sum   += Math.pow(number%10,digits);
+            number = number / 10;
+        }
+        System.out.println("Is this number is Armstrong?"+(sum==temp));
+        /*
+        *       Time Complexity  O(log(N))
+        *       Space Complexity O(1)
+        * */
+    }
+}
