6bebf7651bd0f719b0459b711c92d10f409bc02e
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-08-01 22:18:12 +0530

    feat(math): added divisor

---
+++ src/Basics/Math/Divisor/Solution1.java
@@ -0,0 +1,19 @@
+package Basics.Math.Divisor;
+
+import java.util.Scanner;
+
+public class Solution1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        for(int i = 1; i <= number; i++){
+            if(number % i == 0){
+                System.out.println(i);
+            }
+        }
+        /*
+        *     Time Complexity O(N)
+        *     Space Complexity O(1)
+        * */
+    }
+}
---
+++ src/Basics/Math/Divisor/Solution2.java
@@ -0,0 +1,20 @@
+package Basics.Math.Divisor;
+
+import java.util.Scanner;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        for(int i = 1; i <= Math.sqrt(number); i++){
+            if(number % i == 0){
+                System.out.println(i);
+                if(i != number/i) System.out.println(number/i);
+            }
+        }
+        /*
+         *     Time Complexity O(sqrt(N))
+         *     Space Complexity O(1)
+         * */
+    }
+}
