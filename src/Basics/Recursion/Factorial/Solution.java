34c68719094d6ee040f8c1aeaece32a153c1299f
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2023-08-03 22:13:29 +0530

    feat: factorial of the number

---
+++ src/Basics/Recursion/Factorial/Solution.java
@@ -0,0 +1,17 @@
+package Basics.Recursion.Factorial;
+import java.util.Scanner;
+public class Solution {
+    public static int factorial(int number){
+        if (number == 0) return 1;
+        return number * factorial(number-1);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println("Factorial of the number is "+ factorial(num));
+        /*
+        *   Time Complexity  :- O(N)
+        *   Space Complexity :- O(N)
+        * */
+    }
+}
