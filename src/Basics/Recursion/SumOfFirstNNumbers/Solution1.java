d93a7d9fe538fb00797974c541e061e7800a8c83
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2023-08-03 21:51:02 +0530

    feat(math): sum of first n numbers

---
+++ src/Basics/Recursion/SumOfFirstNNumbers/Solution1.java
@@ -0,0 +1,19 @@
+package Basics.Recursion.SumOfFirstNNumbers;
+
+import java.util.Scanner;
+
+public class Solution1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++){
+            sum += i;
+        }
+        System.out.println("Sum of First N numbers " + sum);
+        /*
+        *   Time Complexity  :- O(N)
+        *   Space Complexity :- O(1)
+        * */
+    }
+}
