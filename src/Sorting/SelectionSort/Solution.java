7aef0c1ca8c92f099e895e81c13f6868d2172ac5
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-08-09 14:43:12 +0530

    feat: selection sort

---
+++ src/Sorting/SelectionSort/Solution.java
@@ -0,0 +1,30 @@
+package Sorting.SelectionSort;
+
+import Utils.DSAUtils;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Solution {
+    static void selectionSort(int[] arr, int size){
+        for(int i = 0; i < size-2; i++){
+            int min = i;
+            for(int j = i+1; j < size-1; j++){
+                if(arr[i]>arr[j]){
+                    min = j;
+                }
+            }
+            DSAUtils.swap(arr, i, min);
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[]{2,1,67,34,89,90};
+        int size = arr.length;
+        selectionSort(arr, size);
+        System.out.println(Arrays.toString(arr));
+        /*
+        *       Time Complexity  :- O(N^2)
+        *       Space Complexity :- O(N)
+        * */
+    }
+}
