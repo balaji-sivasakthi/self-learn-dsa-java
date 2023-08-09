2b39c3528987cf028f9835a97eab9f64540646ae
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-08-09 13:32:21 +0530

    feat: palindrome

---
+++ src/Basics/Recursion/Palindrome/Solution.java
@@ -0,0 +1,21 @@
+package Basics.Recursion.Palindrome;
+
+public class Solution {
+    static boolean isPalindrome(String str, int r, int l){
+        if(str.charAt(r) != str.charAt(l)){
+            return false;
+        }
+        if(r>l){
+            return true;
+        }
+        return isPalindrome(str,r+1, l-1);
+    }
+    public static void main(String[] args) {
+        String temp = "mam";
+        if(isPalindrome(temp, 0, temp.length()-1)){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
