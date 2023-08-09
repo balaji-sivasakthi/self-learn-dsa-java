77fd5e659add09560a9b85f0d9456cfe06be217f
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-08-09 14:42:54 +0530

    feat: DSA utils

---
+++ src/Utils/DSAUtils.java
@@ -0,0 +1,9 @@
+package Utils;
+
+public class DSAUtils {
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
