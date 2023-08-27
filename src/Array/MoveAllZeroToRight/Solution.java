4e21c609d66ca419ec48d39b5439ddd431719879
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-08-27 18:16:24 +0530

    feat: move all zero to right

---
+++ src/Array/MoveAllZeroToRight/Solution.java
@@ -0,0 +1,24 @@
+package Array.MoveAllZeroToRight;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 0, 2, 4, 0, 3};
+        int k = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i]!=0){
+                arr[k] = arr[i];
+                k++;
+            }
+        }
+        for(int j = k; j < arr.length;j++){
+            arr[j] = 0;
+        }
+        System.out.println(Arrays.toString(arr));
+        /*
+        *      Time Complexity  :- O(2N)
+        *      Space Complexity :- O(1)
+        * */
+    }
+}
