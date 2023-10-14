0caa220cb5b79424019642fe02c85b4537dc42e2
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-10-14 19:20:16 +0530

    add dsa utils to print 2d array

--- Utils/DSAUtils.java
+++ Utils/DSAUtils.java
@@ -1,9 +1,16 @@
 package Utils;
 
+import java.util.Arrays;
+
 public class DSAUtils {
     public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+    }
+    public static void print2DArray(int[][] matrix){
+            for (int[] i : matrix){
+                System.out.println(Arrays.toString(i));
+        }
     }
 }
