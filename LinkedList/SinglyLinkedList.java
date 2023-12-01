38ef1eec17a79a0b23b134537f729b8460f5beba
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-12-01 20:45:28 +0530

    practice with singlyLinkList

---
+++ LinkedList/SinglyLinkedList.java
@@ -0,0 +1,63 @@
+package LinkedList;
+
+import java.util.List;
+
+public class SinglyLinkedList {
+    public static class  LinkNode<T>{
+        private final T data;
+        private LinkNode<T> next;
+        public LinkNode(T data){
+            this.data = data;
+            next = null;
+        }
+    }
+    public static void main(String[] args) {
+
+        LinkNode<Integer> head = new LinkNode<>(0);
+        LinkNode<Integer> listNode1 = new LinkNode<>(1);
+        LinkNode<Integer> listNode2 = new LinkNode<>(2);
+        LinkNode<Integer> listNode3 = new LinkNode<>(3);
+
+        head.next = listNode1;
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+
+        //printListNode(head);
+
+        //printListNode(reverseLinkedList(head));
+
+        // getSize of the LinkedList
+        System.out.println("[SIZE] "+ getSize(head));
+    }
+
+    public static <T> void printListNode(LinkNode<T> head){
+        LinkNode<T> current = head;
+        while(current!=null){
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+
+    public static <T> int getSize(LinkNode<T> head){
+        int size = 0;
+        LinkNode<T> current = head;
+        while (current != null){
+            size++;
+            current = current.next;
+        }
+        return size;
+    }
+    public static <T> LinkNode<T> reverseLinkedList(LinkNode<T> head){
+        LinkNode<T> current = head;
+        LinkNode<T> prev = null;
+        LinkNode<T> next = null;
+        while (current!=null){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return head;
+    }
+}
