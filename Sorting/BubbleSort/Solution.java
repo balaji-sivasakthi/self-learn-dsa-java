d6458657eaf3724aeca994682dba46354243585c
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2024-01-15 09:57:27 +0530

    added recursive bubble sort method

--- Sorting/BubbleSort/Solution.java
+++ Sorting/BubbleSort/Solution.java
@@ -17,10 +17,28 @@ public class Solution {
             if (!flag) break;
         }
     }
-
+    public static void recursiveBubbleSort(int[] arr, int n){
+        if(n == 1){
+            return;
+        }
+        for(int i = 0; i < n - 1; i++){
+            int count = 0;
+            if(arr[i] > arr[i+1]){
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                count++;
+            }
+            if(count == 0){
+                return;
+            }
+        }
+        recursiveBubbleSort(arr, n-1);
+    }
     public static void main(String[] args) {
         int[] arr = new int[]{65, 55, 45, 33};
-        bubbleSort(arr);
+//      bubbleSort(arr);
+        recursiveBubbleSort(arr, arr.length);
         System.out.println(Arrays.toString(arr));
         /*
          *       Time Complexity  :- O(N^2)
