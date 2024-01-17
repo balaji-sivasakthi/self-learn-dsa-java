5a6080661702c5f8c5620c4c7a313692c5de16e6
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2024-01-17 22:09:51 +0530

    added quick sort

--- Sorting/QuickSort/Solution.java
+++ Sorting/QuickSort/Solution.java
@@ -1,11 +1,32 @@
 package Sorting.QuickSort;
 
-public class Solution {
-    public static void quickSort(int[] arr) {
+import Utils.DSAUtils;
 
+import java.util.Arrays;
+
+public class Solution {
+    public static  int partion(int[] arr, int low, int high){
+        int pivot = arr[high];
+        int i = low - 1;
+        for(int j = low; j < high; j++){
+            if(arr[j] < pivot){
+                i++;
+                DSAUtils.swap(arr, i, j);
+            }
+        }
+        DSAUtils.swap(arr, i+1, high);
+        return i+1;
+    }
+    public static void quickSort(int[] arr, int low, int high) {
+        if(low >= high) return;
+        int p = partion(arr, low, high);
+        quickSort(arr, 0, p - 1);
+        quickSort(arr, p + 1, high);
     }
 
     public static void main(String[] args) {
-
+        int[] arr = new int[]{ 23, 4, 53, 1, 2, 5, 7};
+        quickSort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
     }
 }
